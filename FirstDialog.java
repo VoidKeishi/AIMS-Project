05faa6361bbd2862a5b464eddf5c9ef14d1aecf9
void_keishi <phananha4@gmail.com>
2023-10-12 09:51:06 +0700

    Complete assignment 2.2.2

---
+++ FirstDialog.java
@@ -0,0 +1,8 @@
+//2.2.2: FirstDialog
+import javax.swing.JOptionPane;
+public class FirstDialog{
+    public static void main(String[] args){
+        JOptionPane.showMessageDialog(null,"Hello world! How are you?");
+        System.exit(0);
+    }
+}
