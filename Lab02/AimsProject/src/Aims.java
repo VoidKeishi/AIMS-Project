a2ebd24bcdc195784eb41ff654e39d7130509441
VoidKeishi <phananha4@gmail.com>
2023-10-28 21:54:03 +0700

    Complete section 10-13

--- Lab02/AimsProject/src/Aims.java
+++ Lab02/AimsProject/src/Aims.java
@@ -2,8 +2,17 @@
 public class Aims {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+        Cart anOrder = new Cart();
+
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+        anOrder.addDigitalVideoDisc(dvd1);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
+        anOrder.addDigitalVideoDisc(dvd2);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
+        anOrder.addDigitalVideoDisc(dvd3);
+
+        anOrder.removeDigitalVideoDisc(dvd3);
+        System.out.println("Total Cost is: " + anOrder.totalCost());
 	}
-
 }
---
+++ Lab02/AimsProject/src/Cart.java
@@ -0,0 +1,4 @@
+public class Cart {
+    public static final int MAX_NUMBER_ORDERED = 20;
+    private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBER_ORDERED];
+}
