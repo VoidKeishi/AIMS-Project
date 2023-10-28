42295161993c4db50d057f4be36bfab0c65d45f8
VoidKeishi <phananha4@gmail.com>
2023-10-28 22:07:18 +0700

    Complete section 14

--- Lab02/AimsProject/src/Cart.java
+++ Lab02/AimsProject/src/Cart.java
@@ -1,4 +1,52 @@
+package classes;
+
 public class Cart {
     public static final int MAX_NUMBER_ORDERED = 20;
     private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBER_ORDERED];
+    private int qtyOrdered = 0;
+
+    public void addDigitalVideoDisc(DigitalVideoDisc disc) {
+        if (qtyOrdered < MAX_NUMBER_ORDERED) {
+            itemsOrdered[qtyOrdered] = disc;
+            qtyOrdered++;
+            System.out.println("The disc has been added");
+        } else {
+            System.out.println("The cart is almost full");
+        }
+    }
+
+    public void addDigitalVideoDisc(DigitalVideoDisc[] dvdList) {
+        if (qtyOrdered + dvdList.length <= MAX_NUMBER_ORDERED) {
+            for (DigitalVideoDisc digitalVideoDisc : dvdList) {
+                itemsOrdered[qtyOrdered] = digitalVideoDisc;
+                qtyOrdered++;
+            }
+            System.out.println("The discs have been added");
+        }
+        else {
+            System.out.println("The cart is almost full");
+        }
+    }
+
+//    public void addDigitalVideoDisc(DigitalVideoDisc... dvd) {
+//        if (qtyOrdered + dvd.length <= MAX_NUMBER_ORDERED) {
+//            for (DigitalVideoDisc digitalVideoDisc : dvd) {
+//                itemsOrdered[qtyOrdered] = digitalVideoDisc;
+//                qtyOrdered++;
+//            }
+//            System.out.println("The discs have been added");
+//        }
+//        else {
+//        System.out.println("The cart is almost full");
+//    }
+//    }
+
+    public void addDigitalVideoDisc(DigitalVideoDisc dvd1, DigitalVideoDisc dvd2) {
+        if (qtyOrdered + 2 <= MAX_NUMBER_ORDERED) {
+            itemsOrdered[qtyOrdered] = dvd1;
+            qtyOrdered++;
+            itemsOrdered[qtyOrdered] = dvd2;
+            qtyOrdered++;
+        }
+    }
 }
