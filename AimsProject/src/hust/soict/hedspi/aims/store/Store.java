d4cb9457594b97eed94acb821aeae45ae9b0b472
VoidKeishi <phananha4@gmail.com>
2023-11-26 18:04:24 +0700

    Complete Section 5 - Store class

---
+++ AimsProject/src/hust/soict/hedspi/aims/store/Store.java
@@ -0,0 +1,16 @@
+package hust.soict.hedspi.aims.store;
+import java.util.ArrayList;
+import hust.soict.hedspi.aims.disc.DigitalVideoDisc;
+
+public class Store {
+    private ArrayList<DigitalVideoDisc> itemsInStore = new ArrayList<DigitalVideoDisc>();
+
+    public void addDVD(DigitalVideoDisc dvd){
+        itemsInStore.add(dvd);
+    }
+
+    public void removeDVD(DigitalVideoDisc dvd){
+        itemsInStore.remove(dvd);
+    }
+
+}
---
+++ AimsProject/src/hust/soict/hedspi/aims/store/StoreTest.java
@@ -0,0 +1,19 @@
+package hust.soict.hedspi.aims.store;
+import hust.soict.hedspi.aims.disc.DigitalVideoDisc;
+
+public class StoreTest {
+    public static void main(String[] args){
+        //Create new Store object
+        Store store = new Store();
+        //Create new DVD object and add to the cart
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
+        // Add DVDs to the store
+        store.addDVD(dvd1);
+        store.addDVD(dvd2);
+        store.addDVD(dvd3);
+        //Remove DVD from the store
+        store.removeDVD(dvd2);
+    }
+}
