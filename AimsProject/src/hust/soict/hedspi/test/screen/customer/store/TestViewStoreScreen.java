e90125b906db02aca52e0aec3faf71c560745f3c
VoidKeishi <phananha4@gmail.com>
2023-12-21 18:47:16 +0700

    Finish section 5

---
+++ AimsProject/src/hust/soict/hedspi/test/screen/customer/store/TestViewStoreScreen.java
@@ -0,0 +1,44 @@
+package hust.soict.hedspi.test.screen.customer.store;
+
+import hust.soict.hedspi.aims.cart.Cart;
+import hust.soict.hedspi.aims.media.CompactDisc;
+import hust.soict.hedspi.aims.media.DigitalVideoDisc;
+import hust.soict.hedspi.aims.media.Track;
+import hust.soict.hedspi.aims.screen.customer.controller.ViewStoreController;
+import hust.soict.hedspi.aims.store.Store;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class TestViewStoreScreen extends Application {
+	private static Store store;
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception{
+		final String STORE_FXML_FILE_PATH = "/hust/soict/hedspi/aims/screen/customer/view/Store.fxml";
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(STORE_FXML_FILE_PATH));
+		ViewStoreController viewStoreController = new ViewStoreController(store);
+		fxmlLoader.setController(viewStoreController);
+		Parent root = fxmlLoader.load();
+		
+		primaryStage.setTitle("Store");
+		primaryStage.setScene(new Scene(root));
+		primaryStage.show();
+	}
+	
+	public static void main(String[] args) {
+		store = new Store();
+		DigitalVideoDisc dvd1 = new DigitalVideoDisc(1,"The Lion King",
+				"Animation","Roger Allers", 87, 19.95f);
+		CompactDisc cd1 = new CompactDisc(2,"Killer", "Queen");
+		Track track1 = new Track("ABC", 3);
+		Track track2 = new Track("DEF", 5);
+		cd1.addTrack(track1);
+		cd1.addTrack(track2);
+		store.addMedia(dvd1);
+		store.addMedia(cd1);
+		launch(args);
+	}
+}
