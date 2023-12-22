2a2d79d9e935acf332cda87950362e53257b1b19
VoidKeishi <phananha4@gmail.com>
2023-12-22 22:16:48 +0700

    Finish section 7

--- AimsProject/src/hust/soict/hedspi/aims/screen/customer/controller/ViewStoreController.java
+++ AimsProject/src/hust/soict/hedspi/aims/screen/customer/controller/ViewStoreController.java
@@ -1,18 +1,25 @@
 package hust.soict.hedspi.aims.screen.customer.controller;
 import java.io.IOException;
 
+import hust.soict.hedspi.aims.cart.Cart;
 import hust.soict.hedspi.aims.store.Store;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
 public class ViewStoreController {
 	private Store store;
-	public ViewStoreController(Store store) {
+	private Cart cart;
+	public ViewStoreController(Store store, Cart cart) {
 		this.store = store;
+		this.cart = cart;
 	}
 
     @FXML
@@ -47,7 +54,18 @@ public class ViewStoreController {
     }
     @FXML
     void btnViewCartPressed(ActionEvent event) {
-
+    	try {
+    		final String CART_FXML_FILE_PATH = "/hust/soict/dsai/aims/screen/customer/view/Cart.fxml";
+    		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(CART_FXML_FILE_PATH));
+    		fxmlLoader.setController(new CartController(store,cart));
+    		Parent root = fxmlLoader.load();
+    		Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+    		stage.setScene(new Scene(root));
+    		stage.setTitle("Cart");
+    		stage.show();
+    	}catch(IOException e) {
+    		e.printStackTrace();
+    	}
     }
 
 }
